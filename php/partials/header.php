dd6433cd186ede116895ba83b7fc6b8d0c2a2b5a
lucasdelbene <lucasdelbene14@gmail.com>
2023-02-09 19:23:58 -0300

    PROYECTO EPICODES

---
+++ php/partials/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+        <div class="logoTituloHeader">
+            <img src="../../img/logoEpicodes.ico" alt="" class="logoHeader">
+            <h2 class='tituloHeader'><a href="./primeraSeccion.php">EPICODES</a></h2>
+        </div>
+
+        <nav class="menuHeader">
+            <input type="checkbox" id="check">
+            <label for="check" class="checkBoton">
+                <i class="menuIconoHeader"><img src="../../img/menu.png" alt=""></i>
+            </label>
+
+            <ul>
+                <li><a href="./primeraSeccion.php" class="seleccionado">INICIO</a></li>
+                <li><a href="./segundaSeccion.php">COMPRAS</a></li>
+                <li><a href="./terceraSeccion.php">ENVIO AUTOMATICO</a></li>
+            </ul>
+        </nav>
+</header>
--- php/secciones/primeraSeccion.php
+++ php/secciones/primeraSeccion.php
@@ -4,34 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="icon" href="../img/logoEpicodes.ico">
-    <title>EPICODES</title>
     <link rel="stylesheet" href="../../css/secciones/primeraSeccion.css">
+    <title>EPICODES</title>
 </head>
 <body>
-    <header class="header">
-
-        <div class="logoTituloHeader">
-            <img src="../../img/logoEpicodes.ico" alt="" class="logoHeader">
-            <h2 class='tituloHeader'><a href="./primeraSeccion.php">EPICODES</a></h2>
-        </div>
-
-        <nav class="menuHeader">
-            <input type="checkbox" id="check">
-            <label for="check" class="checkBoton">
-                <i class="menuIconoHeader"><img src="../../img/menu.png" alt=""></i>
-            </label>
-
-            <ul>
-                <li><a href="./primeraSeccion.php" class="seleccionado">INICIO</a></li>
-                <li><a href="./segundaSeccion.php">COMPRAS</a></li>
-                <li><a href="./terceraSeccion.php">ENVIO AUTOMATICO</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php require '../partials/header.php' ?>
 
     <div>
         <h1 class="tituloPrimeraSeccion">PRIMERA SECCION EPICODES</h1>
