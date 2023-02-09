c119e56c4b79d9a323c1b78a259dfe01c7af13cd
lucasdelbene <lucasdelbene14@gmail.com>
2023-02-09 01:06:00 -0300

    PROYECTO EPICODES

--- php/index.php
+++ php/index.php
@@ -7,28 +7,34 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="icon" href="../img/logoEpicodes.ico">
     <link rel="stylesheet" href="../css/index.css">
     <title>EPICODES</title>
 </head>
 
 <body>
-<body>
+	<header>
+		
+	</header>
+
 	<div class="contenedor">
 		<div class="columna-izquierda">
 			<div class="registro activo" id="registro">
 				<div class="header">
-					<h1>INICIA SESION EN EPICODES</h1>
+					<h1>INICIAR SESION EN EPICODES</h1>
 				</div>
 		
 				<form class="formulario" id="formulario">
-					<label for="nombre">USUARIO</label>
+					<label for="usuario">USUARIO</label>
 					<div class="contenedor-input">
-						<input type="text" id="nombre" name="nombre">
+						<input type="text" id="usuario" name="usuario">
 					</div>
 		
-					<label for="correo">CONTRASEÑA</label>
+					<label for="contraseña">CONTRASEÑA</label>
 					<div class="contenedor-input">
-						<input type="text" id="correo" name="correo">
+						<span class="iconoContraseña"><i class="fa-solid fa-eye-slash"></i></span>
+						<input type="password" id="contraseña" name="contraseña"> 
 					</div>
 					
 					<div class="contenedor-boton">
@@ -38,10 +44,9 @@
 			</div>
 		</div>
 
-		<div class="columna-derecha">
+		<div class="columna-derecha"></div>
 
-		</div>
+		<script src="../js/index.js"></script>
 	</div>
-</body>
 </body>
 </html>
