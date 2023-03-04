5038c6e259b438fb0f988c4d056ea1b577f472ed
lucasdelbene <lucasdelbene14@gmail.com>
2023-03-04 15:41:41 -0300

    PROYECTO EPICODES

--- src/js/secciones/primeraSeccion.js
+++ src/js/secciones/primeraSeccion.js
@@ -26,49 +26,6 @@ tablaPedidos.addEventListener('submit', async(e) =>{
     }catch{
         console.log(error);
     }
-
-    //LEER FILAS//
-    try{
-        const respuesta = await fetch('https://sheet.best/api/sheets/e1fa69e0-a796-4ef6-8120-3172f47c1ff2');
-     
-        const contenido = await respuesta.json();
-        console.log(contenido); 
-    }catch{
-        console.log(error);
-    }
-    
-
-    //ELIMINAR FILAS//
-    try{
-        const respuesta = await fetch('https://sheet.best/api/sheets/e1fa69e0-a796-4ef6-8120-3172f47c1ff2/',{ //PARA ELIMINAR UNA FILA TENGO QUE AGREGAR EL NUMERO DE LA FILA DESPUES DEL /, POR EJEMPLO /0,1,2,3
-            method:'DELETE'
-        });
-     
-        const contenido = await respuesta.json();
-        console.log(contenido); 
-    }catch{
-        console.log(error);
-    }
-    
-
-    //ACTUALIZAR FILAS//
-    try{
-        const respuesta = await fetch('https://sheet.best/api/sheets/e1fa69e0-a796-4ef6-8120-3172f47c1ff2/1',{ //PARA ACTUALIZAR UNA FILA TENGO QUE AGREGAR EL NUMERO DE LA FILA DESPUES DEL /, POR EJEMPLO /0,1,2,3
-            method:'PATCH',
-            mode:'cors',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ //PARA ACTUALIZAR, TENGO QUE PONER EL NOMBRE DE LA COLUMNA y EL VALOR QUE QUIERA ACTUALIZAR
-                'Correo': 'toto@gmail.com'
-            })
-        });
-     
-        const contenido = await respuesta.json();
-        console.log(contenido); 
-    }catch{
-        console.log(error);
-    }
 })
 
 //FUNCION PARA RECARGAR PAGINA y QUE SE CARGUEN LOS DATOS//
