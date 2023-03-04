e59ba30e04e09588311c528571dec1be1ac63a0f
lucasdelbene <lucasdelbene14@gmail.com>
2023-03-04 18:46:47 -0300

    PROYECTO EPICODES

--- src/js/secciones/primeraSeccion.js
+++ src/js/secciones/primeraSeccion.js
@@ -1,4 +1,4 @@
-//CODIGO QUE ESTOY PROBANDO//
+//MUESTRO LOS DATOS DE GOOGLE SHEETS EN UNA TABLA//
 let conexionURL = 'https://sheet.best/api/sheets/0df03a9f-7630-47d5-a648-7f815358d854'
 fetch(conexionURL)
     .then(response => response.json())
@@ -16,83 +16,6 @@ const mostrarPedidos = (data)=>{
 }   
 
 
-
-
-//CODIGO ANTERIOR//
-/*
-const formulario = document.getElementById('tablaPedidos');
-
-tablaPedidos.addEventListener('submit', async(e) =>{
-    e.preventDefault();
-
-    //ESCRIBIR FILAS//
-    try{
-        const respuesta = await fetch('https://sheet.best/api/sheets/e1fa69e0-a796-4ef6-8120-3172f47c1ff2',{
-            method:'POST',
-            mode:'cors',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                'Consola': tablaPedidos.consola.value,
-                'Precio': tablaPedidos.precio.value,
-                'Ventas': tablaPedidos.ventas.value,
-                'Fecha': tablaPedidos.fecha.value
-            })
-        });
-     
-        const contenido = await respuesta.json();
-        console.log(contenido); 
-    }catch{
-        console.log(error);
-    }
-})
-
-//FUNCION PARA RECARGAR PAGINA y QUE SE CARGUEN LOS DATOS//
-window.addEventListener("load", lookforData);
-
-function lookforData(){
-  google.script.run
-  .withSuccessHandler(pedidos=>{
-    let tablaBody = document.getElementById("tbodyPedidos");
-    var template = document.getElementById("rowTemplate");
-    var templateContenido = template.content;
-
-    pedidos.forEach(pedido=>{
-      const tr = templateContenido.cloneNode(true);
-
-      const columnaConsola = tr.querySelector(".templateConsola");
-      const columnaArticulo = tr.querySelector(".templatePrecio");
-      const columnaFecha = tr.querySelector(".templateVentas");
-      const columnaDescripcion = tr.querySelector(".templateFecha");
-
-      columnaConsola.textContent = pedido[0];
-      columnaArticulo.textContent = pedido[1];
-      columnaFecha.textContent = pedido[2];
-      columnaDescripcion.textContent = pedido[3];
-
-      tablaBody.appendChild(tr);
-    })
-  })
-  .getData();
-}
-
-//FUNCIONES doGet() y getData()//
-function doGet(){
-    const html = HtmlService.createTemplateFromFile("primeraSeccion") //NOMBRE DEL ARCHIVO HTML DONDE SE REALIZARA DICHA FUNCION
-    .evaluate();
-    return html;
-}
-  
-function getData(){
-    const SS = SpreadsheetApp.getActiveSpreadsheet();
-    const sheetPedidos = SS.getSheetByName("Pedidos");  //NOMBRE DE LA HOJA EN GOOGLE SHEETS
-    const dataPedidos = sheetPedidos.getDataRange().getDisplayValues();
-    dataPedidos.shift();
-  
-    return dataPedidos;
-}
-*/
 
 
 
